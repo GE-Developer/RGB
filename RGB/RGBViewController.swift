7e84d9dbd6d69c0e7e0650469a5a03b7e0ffec3f
GE-Developer <on-line-misha@mail.ru>
2023-03-24 08:02:50 +0400

    Done

--- RGB/RGBViewController.swift
+++ RGB/RGBViewController.swift
@@ -91,7 +91,10 @@ final class RGBViewController: UIViewController {
             let green: CGFloat = components?[1] ?? 0.0
             let blue: CGFloat = components?[2] ?? 0.0
             
-            hexString = String(format: "#%02lX%02lX%02lX", lroundf(Float(red * 255)), lroundf(Float(green * 255)), lroundf(Float(blue * 255)))
+            hexString = String(format: "#%02lX%02lX%02lX",
+                               lroundf(Float(red * 255)),
+                               lroundf(Float(green * 255)),
+                               lroundf(Float(blue * 255)))
         }
         return "Hex Color: \(hexString)"
      }
